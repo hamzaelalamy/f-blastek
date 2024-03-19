ecb0ef38761a73be5a0f5bf3642becf877d602e1
Younes-sakhnou <younes.sh1234@gmail.com>
2024-03-19 20:12:30 +0000

    adding controllers for the admin and client

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/databaseConfig";
+import adminRoutes from './routes/admin.routes';
+import clientRoutes from './routes/client.routes';
 
 dotenv.config();
 
@@ -10,11 +12,14 @@ const port = process.env.PORT || 3000;
 connectDB(process.env.DB_URL);
 
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({message : "Testing"});
+  res.status(200).json({ message: "Testing" });
 });
+
+app.use('/api', adminRoutes);
+app.use('/api', clientRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
