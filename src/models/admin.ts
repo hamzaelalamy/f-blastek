404baaaf0daeda3615c669396b1aed5994516a99
Younes-sakhnou <younes.sh1234@gmail.com>
2024-03-19 20:11:04 +0000

    adding the admin and client schema

--- src/models/admin.ts
+++ src/models/admin.ts
@@ -1,28 +1,52 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const adminSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    resetPasswordToken: { type: String },
-    resetPasswordExpire: { type: Date }
+interface IAdmin extends mongoose.Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  resetPasswordToken?: string;
+  resetPasswordExpire?: Date;
+}
+
+const adminSchema = new mongoose.Schema<IAdmin>({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required']
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date
 });
 
-const Admin = mongoose.model('Admin', adminSchema);
+adminSchema.pre<IAdmin>('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
 
-module.exports = Admin;
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error: unknown) {
+    return next(error as Error);
+  }
+});
+
+const Admin = mongoose.model<IAdmin>('Admin', adminSchema);
+
+export default Admin;
