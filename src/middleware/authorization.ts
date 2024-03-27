73b375ba1bae309b002de7b157083791129b2cf5
hamzaelalamy <hamza.elalamy1@uit.ac.ma>
2024-03-27 10:48:56 +0000

    removed console.log

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -8,8 +8,6 @@ const secret_key: String | any = process.env.SECRET_KEY;
 const adminAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
   // Get the token from cookies or headers
   const user = req.body.user;
-  // console.log(req.body.user);
-  // console.log(token);
 
   if (!user) {
     return res.status(401).json({ message: 'Unauthorized: No token provided' });
