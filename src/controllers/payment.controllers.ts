df382936182f288d57d1fbc010531634cbd47acf
fatima-elaroussi <elaroussifatima7@gmail.com>
2024-03-30 11:57:14 +0000

    install stripe packegs and add payment controllers

---
+++ src/controllers/payment.controllers.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import Client from "../models/client";
+import Professional from "../models/professional";
+import Payment from "../models/payments";
+import Intervention from "../models/intervention";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+import { isAnyArrayBuffer } from "util/types";
+dotenv.config();
+export const getCheckoutSession = async (req: Request, res: Response) => {
+  const stripe_secret_key: any = process.env.PAYMENT_SECRET_TOKEN;
+  try {
+    const professional = await Professional.findById(req.params.professionalId);
+    const client = await Client.findById(req.body.clientlId);
+
+    const intervention = await Intervention.findById(req.body.interventionId);
+    const stripe = new Stripe(stripe_secret_key);
+    if (!client) {
+      return res.status(404).json({ message: "Client not found" });
+    }
+    if (!professional) {
+      return res.status(404).json({ message: "professional not found" });
+    }
+    if (!intervention) {
+      return res.status(404).json({ message: "intervetion not found" });
+    }
+
+    const email = client.email;
+    const clientId = client.id;
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      success_url: "http://127.0.0.1/paymentSuccess",
+      cancel_url: "http://127.0.0.1/paymentCancel",
+      customer_email: email,
+      client_reference_id: clientId,
+      line_items: [
+        {
+          price_data: {
+            currency: "MAD",
+            product_data: {
+              name: req.body.serviceName,
+              description: professional.bio,
+            },
+            unit_amount: 10000, // Update with actual service amount in cents
+          },
+        },
+      ],
+    });
+    const payment = new Payment({
+      interventionId: intervention.id,
+      clientId: client.id,
+      professionalId: professional.id,
+      amount: req.body.price,
+      gatewayTransactionId: session.id,
+  
+    });
+
+    await payment.save();
+    res.status(200).json({Message : 'Paid successfully'})
+  } catch (error) {
+
+    res.status(500).json({Message:'Error creating checkout session'})
+  }
+};
