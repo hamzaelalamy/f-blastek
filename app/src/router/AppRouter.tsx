c0a636de56161346ec8cc4a38638926242dbc7d6
hamzaelalamy <hamza.elalamy1@uit.ac.ma>
2024-04-23 16:41:20 +0100

    fixed formatting of AppRouter

--- app/src/router/AppRouter.tsx
+++ app/src/router/AppRouter.tsx
@@ -5,34 +5,40 @@ import AddProfessional from "../pages/professional/AddProfessional";
 import LoginForm from "../components/common/LoginForm";
 
 const router = createBrowserRouter([
-    {
-        path:"/backoffice",
-        element: <AdminLayout />,
-        errorElement: <div>Not Found</div>,
-        children:[
-            {
-                index: true,
-                element: <div>Dashboard: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus at placeat porro quidem nihil. Eos quibusdam modi cumque illo corrupti, eum inventore placeat consequatur. Est labore quas ut praesentium voluptatum.</div>
-            },
-            {
-                path: "professionals",
-                element: <Professional/>
-            },
-            {
-                path: "professionals/create",
-                element: <AddProfessional/>
-            },
- 
-        ]
-    },{
-        path:'/login',
-        element:<LoginForm/>
-        
-     }
+  {
+    path: "/backoffice",
+    element: <AdminLayout />,
+    errorElement: <div>Not Found</div>,
+    children: [
+      {
+        index: true,
+        element: (
+          <div>
+            Dashboard: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ducimus at placeat porro quidem nihil. Eos quibusdam modi cumque
+            illo corrupti, eum inventore placeat consequatur. Est labore quas ut
+            praesentium voluptatum.
+          </div>
+        ),
+      },
+      {
+        path: "professionals",
+        element: <Professional />,
+      },
+      {
+        path: "professionals/create",
+        element: <AddProfessional />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginForm />,
+  },
 ]);
 
 const AppRouter = () => {
-    return <RouterProvider router={router}/>
-}
+  return <RouterProvider router={router} />;
+};
 
 export default AppRouter;
