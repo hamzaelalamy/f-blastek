3e080a897e837b2ded75fa406251acdcadbfe345
hamzaelalamy <hamza.elalamy1@uit.ac.ma>
2024-04-23 10:25:21 +0100

    modified add professional form

--- app/src/components/forms/AddProfessional.tsx
+++ app/src/components/forms/AddProfessional.tsx
@@ -22,138 +22,79 @@ function CreateProfessional() {
     },
     password: ''
   };
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState(initialFormData);
+  // const navigate = useNavigate();
+  // const [formData, setFormData] = useState(initialFormData);
 
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setFormData({
-      ...formData,
-      [id]: value
-    });
-  };
+  // const handleChange = (e:React.) => {
+  //   const { id, value } = e.target;
+  //   setFormData({
+  //     ...formData,
+  //     [id]: value
+  //   });
+  // };
 
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await dispatch(actPostProfessionals(formData));
-      setFormData(initialFormData);
-      navigate('/backoffice/professionals');
-    } catch (error) {
-      console.error('Error submitting form:', error);
-    }
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     await dispatch(actPostProfessionals(formData));
+  //     setFormData(initialFormData);
+  //     navigate('/backoffice/professionals');
+  //   } catch (error) {
+  //     console.error('Error submitting form:', error);
+  //   }
+  // };
 
-    return (
-        <section className="max-w-4xl p-3 ml-auto mr-auto text-black bg-white rounded-md shadow-md">
-      <h1 className="text-xl font-bold capitalize">Account settings</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-          <div>
-            <label htmlFor="username">Username</label>
-            <input
-              id="username"
-              type="text"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-            //   value={formData.username}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="emailAddress">Email Address</label>
-            <input
-              id="emailAddress"
-              type="email"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-            //   value={formData.emailAddress}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              type="password"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="passwordConfirmation">Password Confirmation</label>
-            <input
-              id="passwordConfirmation"
-              type="password"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-            //   value={formData.passwordConfirmation}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="color">Color</label>
-            <input
-              id="color"
-              type="color"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-            //   value={formData.color}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="city">Select City</label>
-            <select
-              id="city"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-              value={formData.city}
-              onChange={handleChange}
-            >
-              <option value="Surabaya">Surabaya</option>
-              <option value="Jakarta">Jakarta</option>
-              <option value="Tangerang">Tangerang</option>
-              <option value="Bandung">Bandung</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="range">Range</label>
-            <input
-              id="range"
-              type="range"
-              className="block w-full py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-            //   value={formData.range}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="date">Date</label>
-            <input
-              id="date"
-              type="date"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-            //   value={formData.date}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="textarea">Text Area</label>
-            <textarea
-              id="textarea"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring"
-            //   value={formData.textarea}
-              onChange={handleChange}
-            ></textarea>
-          </div>
+  return (
+    <form className='mr-10'>
+      <div className="grid gap-6 mb-6 md:grid-cols-2">
+        <div>
+          <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900">First name</label>
+          <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required />
         </div>
-
-        <div className="flex justify-end mt-6">
-          <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600">Save</button>
+        <div>
+          <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900">Last name</label>
+          <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Doe" required />
         </div>
-      </form>
-    </section>      );
+        <div>
+          <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900">City</label>
+          <input type="text" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Rabat" required />
+        </div>
+        <div>
+          <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900">Phone number</label>
+          <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+        </div>
+        <div>
+          <label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900">Address</label>
+          <input type="text" id="address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="address" required />
+        </div>
+        <div>
+          <label htmlFor="visitors" className="block mb-2 text-sm font-medium text-gray-900">Hourly Rate</label>
+          <input type="number" id="visitors" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required />
+        </div>
+      </div>
+      <div className="mb-6">
+        <label htmlFor="specialization" className="block mb-2 text-sm font-medium text-gray-900">Specialization</label>
+        <input type="specialization" id="specialization" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Babysitting" required />
+      </div>
+      <div className="mb-6">
+        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email address</label>
+        <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="john.doe@company.com" required />
+      </div>
+      <div className="mb-6">
+        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+        <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required />
+      </div>
+      <div className="flex items-start mb-6">
+        <label htmlFor="remember" className="mr-2 text-sm font-medium text-gray-900 ms-2">Background Check completed </label>
+        <div className="flex items-center h-5">
+          <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
+        </div>
+      </div>
+      <button type="submit" className="text-white bg-[#20B486] hover:bg-[#198f6a] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+    </form>
+  );
 }
 
 export default CreateProfessional;
