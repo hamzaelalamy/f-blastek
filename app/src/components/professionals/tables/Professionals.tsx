f252c9f0a776ca7a1cb40c738842a332cd0d9091
hamzaelalamy <hamza.elalamy1@uit.ac.ma>
2024-04-26 11:44:27 +0100

    fix

--- app/src/components/professionals/tables/Professionals.tsx
+++ app/src/components/professionals/tables/Professionals.tsx
@@ -21,9 +21,9 @@ export default function Professionals() {
     }, [dispatch, records]);
   
     const handleDelete = (userId:React.FormEvent<HTMLFormElement>) => {
-      // dispatch(actDeleteProfessionals(userId))
-      // .then(() => {console.log("User deleted successfully");})
-      // .catch((err) => console.error("Failed to delete user", err));
+      dispatch(actDeleteProfessionals(userId))
+      .then(() => {console.log("User deleted successfully");})
+      .catch((err) => console.error("Failed to delete user", err));
       toast("User deleted successfully", {
         type: "success",
         position: "bottom-center",
