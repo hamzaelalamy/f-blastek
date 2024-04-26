14906b7724fb959f74e5e41c761d290b7b4bfb3d
hamzaelalamy <hamza.elalamy1@uit.ac.ma>
2024-04-26 13:28:24 +0100

    fix

--- app/src/utils/AxiosInstance.tsx
+++ app/src/utils/AxiosInstance.tsx
@@ -9,32 +9,30 @@ const token = data?.data?.token;
 // Create an Axios instance with base URL and default configurations
 const httpClient = axios.create({
   baseURL: LOCAL_URL,
-  timeout: 10000, // Increase the timeout if needed
+  timeout: 10000,
   headers: {
     'Authorization': `Bearer ${token}` // Set the authorization header with the token
   }
 });
 
-// Add request interceptor to modify outgoing requests before they are sent
+
 httpClient.interceptors.request.use(
   (config) => {
-    // You can add additional logic here if needed
     return config;
   },
   (error) => {
-    // Handle request error
     return Promise.reject(error);
   }
 );
 
-// Add response interceptor to handle responses
+
 httpClient.interceptors.response.use(
   (response) => {
-    // You can modify response data here if needed
+
     return response;
   },
   (error) => {
-    // Handle response error
+
     return Promise.reject(error);
   }
 );
