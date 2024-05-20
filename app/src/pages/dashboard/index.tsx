dd3eeace06b5b61514e0b912af356777e605ac0d
Sakhnou <younes.sh1234@gmail.com>
2024-05-20 11:27:47 +0100

    adding changes to dashboard

--- app/src/pages/dashboard/index.tsx
+++ app/src/pages/dashboard/index.tsx
@@ -24,90 +24,90 @@ const Dashboard = () => {
                 </div> */}
             </div>
             <div className="col-span-1  flex  p-4 w-[320px] h-screen ">
-                    <div className="w-full max-w-md p-4 ml-5 bg-gray-100 border border-gray-200 rounded-lg shadow sm:p-8">
-                        <div className="flex items-center justify-between mb-4">
-                            <h5 className="text-xl font-bold leading-none text-gray-900">Latest Customers</h5>
-                            <a href="#" className="text-sm font-medium text-[#20b486] hover:underline">
-                                View all
-                            </a>
-                        </div>
-                        <div className="flow-root">
-                            <ul role="list" className="divide-y divide-gray-200">
-                                <li className="py-3 sm:py-4">
-                                    <div className="flex items-center">
-                                        <div className="flex-shrink-0">
-                                            <img className="w-8 h-8 rounded-full" src="../../assets/img/akil.jpeg" alt="Neil image" />
-                                        </div>
-                                        <div className="flex-1 min-w-0 ms-4">
-                                            <p className="text-sm font-medium text-gray-900 truncate">
-                                                Neil Sims
-                                            </p>
-                                            <p className="text-sm text-gray-500 truncate">
-                                                email@windster.com
-                                            </p>
-                                        </div>
-                                        <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                            $320
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="py-3 sm:py-4">
-                                    <div className="flex items-center ">
-                                        <div className="flex-shrink-0">
-                                            <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image" />
-                                        </div>
-                                        <div className="flex-1 min-w-0 ms-4">
-                                            <p className="text-sm font-medium text-gray-900 truncate">
-                                                Bonnie Green
-                                            </p>
-                                            <p className="text-sm text-gray-500 truncate">
-                                                email@windster.com
-                                            </p>
-                                        </div>
-                                        <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                            $3467
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="py-3 sm:py-4">
-                                    <div className="flex items-center">
-                                        <div className="flex-shrink-0">
-                                            <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Michael image" />
-                                        </div>
-                                        <div className="flex-1 min-w-0 ms-4">
-                                            <p className="text-sm font-medium text-gray-900 truncate">
-                                                Michael Gough
-                                            </p>
-                                            <p className="text-sm text-gray-500 truncate">
-                                                email@windster.com
-                                            </p>
-                                        </div>
-                                        <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                            $67
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="pt-3 pb-0 sm:pt-4">
-                                    <div className="flex items-center ">
-                                        <div className="flex-shrink-0">
-                                            <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Thomas image" />
-                                        </div>
-                                        <div className="flex-1 min-w-0 ms-4">
-                                            <p className="text-sm font-medium text-gray-900 truncate">
-                                                Thomes Lean
-                                            </p>
-                                            <p className="text-sm text-gray-500 truncate">
-                                                email@windster.com
-                                            </p>
-                                        </div>
-                                        <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                            $2367
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+                <div className="w-full max-w-md p-4 ml-5 bg-gray-100 border border-gray-200 rounded-lg shadow sm:p-8">
+                    <div className="flex items-center justify-between mb-4">
+                        <h5 className="text-xl font-bold leading-none text-gray-900">Latest Customers</h5>
+                        <a href="#" className="text-sm font-medium text-[#20b486] hover:underline">
+                            View all
+                        </a>
                     </div>
+                    <div className="flow-root">
+                        <ul role="list" className="divide-y divide-gray-200">
+                            <li className="py-3 sm:py-4">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                        <img className="w-8 h-8 rounded-full" src="../../assets/img/akil.jpeg" alt="Neil image" />
+                                    </div>
+                                    <div className="flex-1 min-w-0 ms-4">
+                                        <p className="text-sm font-medium text-gray-900 truncate">
+                                            Neil Sims
+                                        </p>
+                                        <p className="text-sm text-gray-500 truncate">
+                                            email@windster.com
+                                        </p>
+                                    </div>
+                                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                        $320
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="py-3 sm:py-4">
+                                <div className="flex items-center ">
+                                    <div className="flex-shrink-0">
+                                        <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image" />
+                                    </div>
+                                    <div className="flex-1 min-w-0 ms-4">
+                                        <p className="text-sm font-medium text-gray-900 truncate">
+                                            Bonnie Green
+                                        </p>
+                                        <p className="text-sm text-gray-500 truncate">
+                                            email@windster.com
+                                        </p>
+                                    </div>
+                                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                        $3467
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="py-3 sm:py-4">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                        <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Michael image" />
+                                    </div>
+                                    <div className="flex-1 min-w-0 ms-4">
+                                        <p className="text-sm font-medium text-gray-900 truncate">
+                                            Michael Gough
+                                        </p>
+                                        <p className="text-sm text-gray-500 truncate">
+                                            email@windster.com
+                                        </p>
+                                    </div>
+                                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                        $67
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="pt-3 pb-0 sm:pt-4">
+                                <div className="flex items-center ">
+                                    <div className="flex-shrink-0">
+                                        <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Thomas image" />
+                                    </div>
+                                    <div className="flex-1 min-w-0 ms-4">
+                                        <p className="text-sm font-medium text-gray-900 truncate">
+                                            Thomes Lean
+                                        </p>
+                                        <p className="text-sm text-gray-500 truncate">
+                                            email@windster.com
+                                        </p>
+                                    </div>
+                                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                        $2367
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
